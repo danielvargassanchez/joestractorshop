9f27e5539770575a0244f59ab80c5fc6455d3a36
Daniel <danielvargassanchez@hotmail.com>
2020-12-19 13:13:24 -0600

    all completed

--- javascript/app.js
+++ javascript/app.js
@@ -1,88 +1,77 @@
-
-
-var percent=0;
+var percent = 0;
 try {
-    
-var slideBtnRight= document.getElementById("slide-btn-right");
-var slideBtnLeft= document.getElementById("slide-btn-left");
-var slide= document.getElementById("slides");
-var slideH1= document.getElementById("slide-h1");
-var slideH2= document.getElementById("slide-h2");
-    
+  var slideBtnRight = document.getElementById("slide-btn-right");
+  var slideBtnLeft = document.getElementById("slide-btn-left");
+  var slide = document.getElementById("slides");
+  var slideH1 = document.getElementById("slide-h1");
+  var slideH2 = document.getElementById("slide-h2");
 
-var categoriesContainer= document.getElementById("filter-by-categorie");
-var btnCategories= document.getElementById("show-categories");
+  var categoriesContainer = document.getElementById("filter-by-categorie");
+  var btnCategories = document.getElementById("show-categories");
 
-
-
-btnCategories.addEventListener("click", ()=>{
+  btnCategories.addEventListener("click", () => {
     categoriesContainer.classList.toggle("hide");
     btnCategories.classList.toggle("down-button");
-});
-} catch (error) {
-    
-}
-
-
-
-
-//products events
-
+  });
+} catch (error) {}
 
 // Index events and keyframes
 try {
-    document.addEventListener("DOMContentLoaded",function(event){
-        addKeyframes();
-    });
-} catch (error) { 
-}
-
-try{
-    slideH1.addEventListener("animationend", (e)=>{
-        slideH1.removeAttribute("class", "animation-slide-h1");
-    })
-}
-catch(error){
-}
+  document.addEventListener("DOMContentLoaded", function (event) {
+    addKeyframes();
+  });
+} catch (error) {}
 
 try {
-    slideH2.addEventListener("animationend", (e)=>{
-        slideH2.removeAttribute("class", "animation-slide-h2");
-    })    
-} catch (error) {
-}
-
+  slideH1.addEventListener("animationend", (e) => {
+    slideH1.removeAttribute("class", "animation-slide-h1");
+  });
+} catch (error) {}
 
 try {
-    slideBtnLeft.addEventListener("click", ()=>{
-        if(percent!=0){
-            percent=percent-100;
-            slide.style.marginLeft="-"+percent+"%";
-            addKeyframes();
-        }
-    })
-} catch (error) {
-}
+  slideH2.addEventListener("animationend", (e) => {
+    slideH2.removeAttribute("class", "animation-slide-h2");
+  });
+} catch (error) {}
 
 try {
-    slideBtnRight.addEventListener("click", ()=>{
-        if(percent!=200){
-            percent=percent+100;
-            slide.style.marginLeft="-"+percent+"%";
-            addKeyframes();
-        }else{
-            percent=0
-            slide.style.marginLeft="-"+percent+"%";
-            addKeyframes();
-        }
-    })
-} catch (error) {
-}
-
-try {
-    function addKeyframes(){
-        slideH1.setAttribute("class", "animation-slide-h1");
-        slideH2.setAttribute("class", "animation-slide-h2");
+  slideBtnLeft.addEventListener("click", () => {
+    if (percent != 0) {
+      percent = percent - 100;
+      slide.style.marginLeft = "-" + percent + "%";
+      addKeyframes();
     }
-} catch (error) {
-}
+  });
+} catch (error) {}
+
+try {
+  slideBtnRight.addEventListener("click", () => {
+    if (percent != 200) {
+      percent = percent + 100;
+      slide.style.marginLeft = "-" + percent + "%";
+      addKeyframes();
+    } else {
+      percent = 0;
+      slide.style.marginLeft = "-" + percent + "%";
+      addKeyframes();
+    }
+  });
+} catch (error) {}
+
+try {
+  function addKeyframes() {
+    slideH1.setAttribute("class", "animation-slide-h1");
+    slideH2.setAttribute("class", "animation-slide-h2");
+  }
+} catch (error) {}
+
+addEventListener("DOMContentLoaded", () => {
+  const btnMenu = document.getElementById("btn-menu");
+  const navbar = document.getElementById("navbar");
+  if (btnMenu && navbar) {
+    btnMenu.addEventListener("click", () => {
+      console.log("evento");
+      navbar.classList.toggle("show");
+    });
+  }
+});
